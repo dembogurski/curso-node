03638a87fc8bfbdac861a5343f9825646926aec4
dembogurski <dembogurski@gmail.com>
2022-12-09 18:33:26 -0300

    Ejemplo de Exportar modulo

---
+++ cursos.js
@@ -0,0 +1,44 @@
+let infoCursos = {
+   'programacion':[
+    {
+        id:1,
+        titulo : 'Aprende Python',
+        lenguaje: 'Python',
+        vistas: 15000,
+        nivel: 'basico'
+    },
+    {
+        id:2,
+        titulo : 'Python intermedio',
+        lenguaje: 'Python',
+        vistas: 15353,
+        nivel: 'intermedio'
+    },
+    {
+        id:3,
+        titulo : 'Aprende Javascript',
+        lenguaje: 'Javascript',
+        vistas: 15000,
+        nivel: 'basico'
+    }
+   ],
+   'matematicas':[
+    {
+        id:1,
+        titulo : 'Aprende Calculo',
+        tema: 'calculo',
+        vistas: 1600,
+        nivel: 'basico'
+    },
+    {
+        id:2,
+        titulo : 'Aprende Algebra',
+        tema: 'algebra',
+        vistas: 2000,
+        nivel: 'intermedio'
+    }
+   ]
+}
+
+module.exports.infoCursos = infoCursos;
+
